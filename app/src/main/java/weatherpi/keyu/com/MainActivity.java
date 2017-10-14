3e71161c1bd37cb2aac4ef6f4d6503ab8b769893
focuschan <1057951706@qq.com>
2017-10-14 14:59:56 +0800

    The app now can update today`s weather successfully.

--- app/src/main/java/weatherpi/keyu/com/MainActivity.java
+++ app/src/main/java/weatherpi/keyu/com/MainActivity.java
@@ -15,8 +15,7 @@ import org.xmlpull.v1.XmlPullParserFactory;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
 
 import weatherpi.keyu.com.entity.WeatherInfo;
 import weatherpi.keyu.com.utils.Constant;
